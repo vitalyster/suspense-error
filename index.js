6dc224ed1836a0f67640ef78ad8cfad050bf77fb
Vitaly Takmazov <vitalyster@gmail.com>
2018-12-07 17:07:32 +0300

    Version with dynamic import

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 
-import Main from './Main';
+function LoadingView(props) {
+    return (
+        <>
+            <div id="header">
+                <p>Header loading...</p>
+            </div>
+            <div id="wrapper">
+                <div id="content">
+                    <p>Content loading...</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+const Main = lazy(() => import('./Main'));
+
+const App = () => (
+    <Suspense fallback={LoadingView()}>
+        <Main />
+    </Suspense>
+);
 
 let container = document.createElement('div');
-ReactDOM.render(<Main />, container);
+ReactDOM.render(<App />, container);
 let app = document.getElementById('app');
 app.appendChild(container);
 let renderedHeader = container.querySelector('#header');
